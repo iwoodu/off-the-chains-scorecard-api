455a6074ea09ece527ec4de2619e4de8310b1896
David <david.wood@tutanota.com>
2020-01-19 09:54:18 -0500

    Basic project structure

---
+++ src/main/java/com/rhitm/scorecard/OffTheChainsScorecardApiApplication.java
@@ -0,0 +1,13 @@
+package com.rhitm.scorecard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffTheChainsScorecardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffTheChainsScorecardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhitm/scorecard/OffTheChainsScorecardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhitm.scorecard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffTheChainsScorecardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
